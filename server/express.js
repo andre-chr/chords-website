dcbb455f84cf9953834ad497b4397feb7da64e23
andre-chr <andrechr98@gmail.com>
2017-12-20 15:55:35 +0700

    init basic server

---
+++ server/express.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('*', (req, res) => {
+	app.sendFile(path.join(__dirname, 'dist/index.html'));
+});
+
+/**
+ * Get port from environment and store in Express.
+ */
+const port = process.env.PORT || '3000';
+app.set('port', port);
+
+/**
+ * Create HTTP server.
+ */
+const server = http.createServer(app);
+
+/**
+ * Listendi on provided port, on all network interfaces.
+ */
+server.listen(port, () => console.log(`Server running on localhost:${port}`));
